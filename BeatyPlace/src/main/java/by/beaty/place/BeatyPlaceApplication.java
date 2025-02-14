09f4aafcc2bb19459eb875fe4160035d4d391b31
a.miadzvedzeu <arthaos14@gmail.com>
2025-02-14 21:43:35 +0300

    Init commit

---
+++ BeatyPlace/src/main/java/by/beaty/place/BeatyPlaceApplication.java
@@ -0,0 +1,13 @@
+package by.beaty.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatyPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatyPlaceApplication.class, args);
+    }
+
+}
---
+++ BeatyPlace/src/test/java/by/beaty/place/BeatyPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.beaty.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatyPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
