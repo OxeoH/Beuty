08133356d8eb52ea5267fc690a2a59a58587e3d0
a.miadzvedzeu <arthaos14@gmail.com>
2025-04-15 01:17:31 +0300

    Добавил тесты для сервиса записей для метода проверки записи мастера

--- BeatyPlace/src/test/java/by/beaty/place/service/AppointmentServiceImplTest.java
+++ BeatyPlace/src/test/java/by/beaty/place/service/AppointmentServiceImplTest.java
@@ -1,6 +1,7 @@
 package by.beaty.place.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -340,5 +341,57 @@ class AppointmentServiceImplTest {
 
         // THEN
         verify(appointmentRepository, times(1)).updateStatus(appointmentId, newStatus);
+    }
+
+    @Test
+    void hasMasterAppointmentById_shouldReturnTrue_whenAppointmentExists() {
+        // GIVEN
+        Long appointmentId = 1L;
+        Long masterId = 2L;
+
+        when(appointmentRepository.hasMasterAppointmentById(appointmentId, masterId)).thenReturn(true);
+
+        // WHEN
+        boolean result = appointmentService.hasMasterAppointmentById(appointmentId, masterId);
+
+        // THEN
+        assertTrue(result);
+        verify(appointmentRepository, times(1)).hasMasterAppointmentById(appointmentId, masterId);
+    }
+
+    @Test
+    void hasMasterAppointmentById_shouldReturnFalse_whenAppointmentDoesNotExist() {
+        // GIVEN
+        Long appointmentId = 1L;
+        Long masterId = 2L;
+
+        when(appointmentRepository.hasMasterAppointmentById(appointmentId, masterId)).thenReturn(false);
+
+        // WHEN
+        boolean result = appointmentService.hasMasterAppointmentById(appointmentId, masterId);
+
+        // THEN
+        assertFalse(result);
+        verify(appointmentRepository, times(1)).hasMasterAppointmentById(appointmentId, masterId);
+    }
+
+    @Test
+    void hasMasterAppointmentById_shouldThrowException_whenIdIsNull() {
+        // GIVEN
+        Long masterId = 2L;
+
+        // WHEN | THEN
+        assertThrows(IllegalArgumentException.class, () ->
+                appointmentService.hasMasterAppointmentById(null, masterId));
+    }
+
+    @Test
+    void hasMasterAppointmentById_shouldThrowException_whenMasterIdIsNull() {
+        // GIVEN
+        Long appointmentId = 1L;
+
+        // WHEN | THEN
+        assertThrows(IllegalArgumentException.class, () ->
+                appointmentService.hasMasterAppointmentById(appointmentId, null));
     }
 }
